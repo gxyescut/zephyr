b9ac4ebe39ca52a1bfe879d9109bedcc59f89268
Filip Kokosinski <fkokosinski@antmicro.com>
2021-08-19 12:09:23 +0200

    wip: hifive1 on renode

--- boards/riscv/hifive1/board.cmake
+++ boards/riscv/hifive1/board.cmake
@@ -1,15 +1,18 @@
 # SPDX-License-Identifier: Apache-2.0
 
-set(EMU_PLATFORM qemu)
+set(EMU_PLATFORM renode)
+set(RENODE_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/support/hifive1.resc)
 
-set(QEMU_binary_suffix riscv32)
-set(QEMU_CPU_TYPE_${ARCH} riscv32)
-
-set(QEMU_FLAGS_${ARCH}
-  -nographic
-  -machine sifive_e
-  )
-
-board_set_debugger_ifnset(qemu)
-board_set_flasher_ifnset(hifive1)
-board_finalize_runner_args(hifive1)
+#set(EMU_PLATFORM qemu)
+#
+#set(QEMU_binary_suffix riscv32)
+#set(QEMU_CPU_TYPE_${ARCH} riscv32)
+#
+#set(QEMU_FLAGS_${ARCH}
+#  -nographic
+#  -machine sifive_e
+#  )
+#
+#board_set_debugger_ifnset(qemu)
+#board_set_flasher_ifnset(hifive1)
+#board_finalize_runner_args(hifive1)
