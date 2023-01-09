68e8fa4c623e723419fd9f9756d2bd910e83289a
Pawel Czarnecki <pczarnecki@antmicro.com>
2023-01-09 15:08:08 +0100

    drivers: watchdog: gecko: enable watchdog clock
    Clock needs to be enabled after it was selected
    
    Signed-off-by: Pawel Czarnecki <pczarnecki@antmicro.com>

--- drivers/watchdog/wdt_gecko.c
+++ drivers/watchdog/wdt_gecko.c
@@ -259,6 +259,8 @@ static int wdt_gecko_init(const struct device *dev)
 	CMU_ClockEnable(config->clock, true);
 #else
 	CMU_ClockSelectSet(config->clock, cmuSelect_ULFRCO);
+	/* Enable Watchdog clock. */
+	CMU_ClockEnable(cmuClock_WDOG0, true);
 #endif
 
 	/* Enable IRQs */
