e3e17f8c6b368f9cb30ee76f087e4cfadfd8c914
Aaron Ye <aye@ambiq.com>
2023-08-25 17:03:21 +0800

    Test the Apollo4p GPIO73 (Bluetooth Controller IRQ) interrupt.

--- samples/basic/blinky/src/main.c
+++ samples/basic/blinky/src/main.c
@@ -12,12 +12,23 @@
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
+#define IRQ_NODE  DT_ALIAS(bleirq)
 
 /*
  * A build error on this line means your board is unsupported.
  * See the sample documentation for information on how to fix this.
  */
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec irq = GPIO_DT_SPEC_GET(IRQ_NODE, gpios);
+
+static struct gpio_callback	gpio_cb;
+
+static void irq_isr(const struct device *unused1,
+		       struct gpio_callback *unused2,
+		       uint32_t unused3)
+{
+	printk(".\n");
+}
 
 int main(void)
 {
@@ -30,6 +41,30 @@ int main(void)
 	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
 	if (ret < 0) {
 		return 0;
+	}
+
+	if (!gpio_is_ready_dt(&irq)) {
+		printk("Device not ready\n");
+		return 0;
+	}
+
+	ret = gpio_pin_configure_dt(&irq, GPIO_INPUT);
+	if (ret < 0) {
+		printk("IRQ PIN Config fail\n");
+		return 0;
+	}
+
+	gpio_init_callback(&gpio_cb, irq_isr, BIT(irq.pin));
+	ret = gpio_add_callback(irq.port, &gpio_cb);
+	if (ret) {
+		printk("Add callback fail\n");
+		return ret;
+	}
+
+	ret = gpio_pin_interrupt_configure_dt(&irq, GPIO_INT_EDGE_TO_ACTIVE);
+	if (ret) {
+		printk("Interrupt config fail\n");
+		return ret;
 	}
 
 	while (1) {
